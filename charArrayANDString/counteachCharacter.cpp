fadd57391414dd0a7ef6add981d3abe93224c743
rajesh <852tutukumar@gmail.com>
2024-12-31 23:29:27 +0530

    count

--- charArrayANDString/counteachCharacter.cpp
+++ charArrayANDString/counteachCharacter.cpp
@@ -1,5 +1,31 @@
 
 
+// #include <iostream>
+// using namespace std;
+
+// void countElementsWithASCII(const string& str) {
+//     // Array to store counts for each ASCII value
+//     int asciiCoun[256] = {0};
+
+//     // Iterate through the string to count each character
+//     for (char ch : str) {
+//         asciiCoun[(int)ch]++;
+        
+//         // Print the character and its count as soon as we encounter it
+//         cout << "Character: " << ch 
+//              << " (ASCII: " << (int)ch 
+//              << "), Count: " << asciiCoun[int(ch)] << endl;
+//     }
+// }
+
+// int main() {
+//     string input = "hello";
+//     countElementsWithASCII(input);
+//     return 0;
+// }
+
+
+
 #include <iostream>
 using namespace std;
 
@@ -9,12 +35,14 @@ void countElementsWithASCII(const string& str) {
 
     // Iterate through the string to count each character
     for (char ch : str) {
-        asciiCoun[(int)ch]++;
+        asciiCoun[ch]++;
         
+    }
+    for(int i=0;i<256;i++){
         // Print the character and its count as soon as we encounter it
-        cout << "Character: " << ch 
-             << " (ASCII: " << (int)ch 
-             << "), Count: " << asciiCoun[(int)ch] << endl;
+        if(asciiCoun[i] > 0){
+            cout << char(i) << " : " << asciiCoun[i] << endl;
+        }
     }
 }
 
@@ -23,3 +51,4 @@ int main() {
     countElementsWithASCII(input);
     return 0;
 }
+
